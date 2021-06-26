1aa44e5c8bdf1c6cab1d7b4bc3c393ab213ac8b1
olychkam <olkaaamartynova@gmail.com>
2021-06-26 10:43:17 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import * as axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const getUsers = () => {
+        axios.get ("http://localhost:7542/users").then (res => {
+            setUsers (res.data);
+        })
+    }
+    const addHandler = () => {
+        axios.post ("http://localhost:7542/users").then (res => {
+            getUsers ();
+        })
+    }
+    const [users, setUsers] = useState ([]);
+    useEffect (() => {
+        getUsers ();
+    }, [])
+    return (
+        <div>
+            <button onClick={addHandler}>ADD</button>
+            <div>{users.map (u => <div>{u.name}</div>)}</div>
+        </div>
+    );
 }
 
 export default App;
